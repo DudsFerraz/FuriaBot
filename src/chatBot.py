1b0f9b345c1c196a7316f725fc56c7aa9a6a86ed
Eduardo Ferraz <dudiferraz@gmail.com>
2025-04-28 16:19:55 -0300

    update to be compatible with Docker

--- src/chatBot.py
+++ src/chatBot.py
@@ -54,36 +54,44 @@ def format_date(unix):
 
 def stealth_html_getter(url: str) -> BeautifulSoup:
     chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--disable-3d-apis")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                                "Chrome/124.0.0.0 Safari/537.36")
+                                 "AppleWebKit/537.36 (KHTML, like Gecko) "
+                                 "Chrome/124.0.0.0 Safari/537.36")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
 
     global my_service
-    browser = webdriver.Chrome(service=my_service,options=chrome_options)
+    browser = webdriver.Chrome(service=my_service, options=chrome_options)
     browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
         "source": """
         Object.defineProperty(navigator, 'webdriver', {get: () => undefined});
-        """})
+        """
+    })
     
     browser.get(url)
     time.sleep(3)
 
     try:
-        allowCookies = browser.find_element("id","CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
+        allowCookies = browser.find_element("id", "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
         allowCookies.click()
         time.sleep(1)
     except:
         pass
 
-    soup = BeautifulSoup(browser.page_source,"html.parser")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
     browser.quit()
     return soup
+
 
 
 def start_background_updates():
