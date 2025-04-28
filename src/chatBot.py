7222ec8468046e83e5c3debaa90f3f437e0612c7
Eduardo Ferraz <dudiferraz@gmail.com>
2025-04-28 10:55:00 -0300

    fix: force chromedriver usage

--- src/chatBot.py
+++ src/chatBot.py
@@ -10,6 +10,7 @@ import time
 from selenium.webdriver.chrome.options import Options
 import threading
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -57,14 +58,14 @@ def stealth_html_getter(url: str) -> BeautifulSoup:
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--remote-debugging-port=9222") # necessário no Alpine
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                                  "AppleWebKit/537.36 (KHTML, like Gecko) "
                                  "Chrome/124.0.0.0 Safari/537.36")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
 
-    service = Service("/usr/bin/chromedriver")
+    service = Service(ChromeDriverManager().install())
     browser = webdriver.Chrome(service=service, options=chrome_options)
 
     browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
